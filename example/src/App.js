e0f88635ae9fc28f8236a2458636e5e69775463b
Andrew McCloud <andrew@amccloud.com>
2017-03-15 04:11:47 -0700

    Add Spinner

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,7 @@
 /* @jsx Async.createElement */
 
 import React from 'react';
+import Spinner from 'react-svg-spinner';
 import {Async, Await} from 'await-component';
 import {delay} from './utils';
 
@@ -8,7 +9,9 @@ const Container = import('./Container').then(delay(2000));
 const Sum = import('./Sum');
 
 export default () => (
-  <Await loading={<div>Loading...</div>} error={<div>ERROR!</div>}>
+  <Await
+    loading={<Spinner size={50} thickness={1} />}
+    error={<div>ERROR!</div>}>
     <Container>
       <Sum a={1} b={2} />
       <Sum a={2} b={2} />
