4a5877ccf9260aa987ef3382d40987bcc97d778c
Andrew McCloud <andrew@amccloud.com>
2017-03-16 21:52:55 -0700

    Add `preload` API

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [v0.1.0-beta.4]
+> master
+
+## [v0.1.0-beta.3]
+> Mar 16, 2017
+
+- Added `preload` API
--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import Async from './Async';
 import Await from './Await';
 
 export {Async, Await};
+export function preload(component) {
+  if (component.load) {
+    component.load();
+  }
+
+  return component;
+}
